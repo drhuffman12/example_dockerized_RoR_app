d47db7925760e9f97f17911d66fdbc97f0a2e29b
Daniel Huffman <drhuffman12@gmail.com>
2022-02-09 03:58:14 -0500

    bump version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,6 +3,6 @@
 module Myapp
   class Application
     GIT_COMMIT = `git log --oneline | head -1`.freeze
-    VERSION = "0.1.1(#{GIT_COMMIT.split.first})".freeze
+    VERSION = "0.1.2(#{GIT_COMMIT.split.first})".freeze
   end
 end
